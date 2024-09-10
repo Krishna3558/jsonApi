2e31e95d9d88fb20f7e561d1294b26d4f06f46a3
Krishna3558 <rajeevmishra6066@gmail.com>
2024-09-10 16:50:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState , useEffect } from 'react'
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching data: {error.message}</p>;
+
+  return (
+    <div>
+      <h1 className=' text-center font-bold text-2xl mb-10 pt-6'>Posts List</h1>
+      <ol>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
+}
+
+export default App
